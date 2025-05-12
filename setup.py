1606c4de11e96db513d1d376099b45404c5834a2
tejnica <tejnica@gmail.com>
2025-05-13 00:34:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='creatoria-core',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
-        'numpy'
+        'numpy',
     ],
     entry_points={
         'console_scripts': [
-            'creatoria-cli=creatoria_core.cli:main',
+            'creatoria-cli = creatoria_core.cli:main',
         ],
     },
 )
